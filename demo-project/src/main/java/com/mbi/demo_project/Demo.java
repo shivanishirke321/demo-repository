229876b475e4f9b60f788baa760bacc2bfd0e74f
Shivani Shirke <shivanishirke321@gmail.com>
2025-03-13 13:06:23 +0530

    I have deleted the content created by pandurang

--- demo-project/src/main/java/com/mbi/demo_project/Demo.java
+++ demo-project/src/main/java/com/mbi/demo_project/Demo.java
@@ -5,6 +5,5 @@ public void send() {
 	System.out.println("Trying to push");
 	
 	System.out.println("changes made by pandurang and shivani");
-	System.out.println("added another branch changes made by pandurang");
 }
 }
